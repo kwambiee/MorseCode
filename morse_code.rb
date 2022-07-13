be65d3ed106c4cfa9bf5047df62b2869a16f50e9
kwambiee <kwambokaj2.jk@gmail.com>
2022-07-13 15:42:35 +0300

    Fix linters

--- morse_code.rb
+++ morse_code.rb
@@ -1,62 +1,62 @@
-@morse_dictionary= {
-  "a" => ".-",
-  "b" => "-...",
-  "c" => "-.-.",
-  "d" => "-..",
-  "e" => ".",
-  "f" => "..-.",
-  "g" => "--.",
-  "h" => "....",
-  "i" => "..",
-  "j" => ".---",
-  "k" => "-.-",
-  "l" => ".-..",
-  "m" => "--",
-  "n" => "-.",
-  "o" => "---",
-  "p" => ".--.",
-  "q" => "--.-",
-  "r" => ".-.",
-  "s" => "...",
-  "t" => "-",
-  "u" => "..-",
-  "v" => "...-",
-  "w" => ".--",
-  "x" => "-..-",
-  "y" => "-.--",
-  "z" => "--..",
-  " " => " ",
-  "1" => ".----",
-  "2" => "..---",
-  "3" => "...--",
-  "4" => "....-",
-  "5" => ".....",
-  "6" => "-....",
-  "7" => "--...",
-  "8" => "---..",
-  "9" => "----.",
-  "0" => "-----"
+@morse_dictionary = {
+  'a' => '.-',
+  'b' => '-...',
+  'c' => '-.-.',
+  'd' => '-..',
+  'e' => '.',
+  'f' => '..-.',
+  'g' => '--.',
+  'h' => '....',
+  'i' => '..',
+  'j' => '.---',
+  'k' => '-.-',
+  'l' => '.-..',
+  'm' => '--',
+  'n' => '-.',
+  'o' => '---',
+  'p' => '.--.',
+  'q' => '--.-',
+  'r' => '.-.',
+  's' => '...',
+  't' => '-',
+  'u' => '..-',
+  'v' => '...-',
+  'w' => '.--',
+  'x' => '-..-',
+  'y' => '-.--',
+  'z' => '--..',
+  ' ' => ' ',
+  '1' => '.----',
+  '2' => '..---',
+  '3' => '...--',
+  '4' => '....-',
+  '5' => '.....',
+  '6' => '-....',
+  '7' => '--...',
+  '8' => '---..',
+  '9' => '----.',
+  '0' => '-----'
 }
 
 def decode_char(character)
-  @current_morse_character=@morse_dictionary.invert[character]
+  @current_morse_character = @morse_dictionary.invert[character]
   @current_morse_character.upcase
 end
 
-def decode_word(word) 
-  @entire_word='' 
-  @characters=word.split 
-  @characters.each do |character| 
-    @entire_word += decode_char(character) 
+def decode_word(word)
+  @entire_word = ''
+  @characters = word.split
+  @characters.each do |character|
+    @entire_word += decode_char(character)
   end
-  print "#{@entire_word}" 
-end 
+  print @entire_word.to_s
+end
 
-def decode_sentence(sentence) 
-  @entire_sentence=sentence.split(' ') 
-  @entire_sentence.each do |message| 
-    decode_word(message) 
-  end 
+def decode_sentence(sentence)
+  @entire_sentence = sentence.split
+  @entire_sentence.each do |message|
+    decode_word(message)
+  end
 end
 
 decode_sentence('.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...')
